37f362f0a8a830218c92071c31d14c06a4850906
Itallo Edward <128102018+iT4lloEdw@users.noreply.github.com>
2025-06-02 21:13:58 -0300

    Add files via upload

--- POO/Funcionario.java
+++ POO/Funcionario.java
@@ -2,25 +2,28 @@ public class Funcionario {
 	String nome;
 	String cargo;
 	int idade;
-	public static void main (String[] args) {
 
-		Funcionario funcionario1 = new Funcionario();
-		funcionario1.nome = "Itallo";
-		funcionario1.cargo = "Desenvolvedor Jr.";
-		funcionario1.idade = 20;
+	Funcionario(int idadeInit, String cargoInit, String nomeInit ) { // Sobrecarga de Construtores
+		nome = nomeInit;
+		cargo = cargoInit;
+		idade = idadeInit;
+	}
+
+	Funcionario(String nomeInit) {
+
+	}
+
+	Funcionario() { // Sobrecarga de Construtores
+	}
+
+	public static void main(String[] args) {
+
+		Funcionario funcionario = new Funcionario(20, "Desenvolvedor Jr", "Itallo");
 		
-		Funcionario funcionario2 = new Funcionario();
-		funcionario2.nome = "Zeca";
-		funcionario2.cargo = "Gerente";
-		funcionario2.idade = 32;
 
-		System.out.println("Nome: " + funcionario1.nome);
-		System.out.println("Cargo: " + funcionario1.cargo);
-		System.out.println("Idade: " + funcionario1.idade);
-		System.out.println("-----------------------------");
-		System.out.println("Nome: " + funcionario2.nome);
-		System.out.println("Cargo: " + funcionario2.cargo);
-		System.out.println("Idade: " + funcionario2.idade);
-
-	}	
+		System.out.println("Nome: " + funcionario.nome);
+		System.out.println("Cargo: " + funcionario.cargo);
+		System.out.println("Idade: " + funcionario.idade);
+		
+	}
 }
